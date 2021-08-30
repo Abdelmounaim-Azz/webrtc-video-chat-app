9c6bd8003ba1592ca25b9ecff65abe434b4082d0
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-08-30 14:21:30 +0100

    render html content

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
