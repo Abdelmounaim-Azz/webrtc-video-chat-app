571e73d15e0812244d0638844588d4e148187648
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-08-31 15:37:54 +0100

    updating personal code in the ui using socket id

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,5 @@
 import * as store from "./store.js";
+import * as wss from "./wss.js";
 
 const socket = io("/");
-
-socket.on("connect", () => {
-  console.log("succesfully connected to socket.io server");
-  console.log(socket.id);
-  store.setSocketId(socket.id);
-});
+wss.registerSocketEvents(socket)
---
+++ public/js/ui.js
@@ -0,0 +1,4 @@
+export const updatePersonalCode = (personalCode) => {
+  const personalCodepara = document.getElementById("personal_code_paragraph");
+  personalCodepara.innerHTML = personalCode;
+};
