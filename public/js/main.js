264c9f24cee2a958552c44e6fef1ff020a4c2840
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-08-31 15:49:03 +0100

    adding copy btn functionnality

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 import * as store from "./store.js";
 import * as wss from "./wss.js";
 
+//Initialize the socket io connection
 const socket = io("/");
-wss.registerSocketEvents(socket)
+wss.registerSocketEvents(socket);
+
+//register event for the personal code copy btn
+const personalCodeCopyBtn=document.getElementById('personal_code_copy_button');
+personalCodeCopyBtn.addEventListener('click',()=>{
+  const personalCode=store.getState().socketId;
+  navigator.clipboard && navigator.clipboard.writeText(personalCode);
+})
