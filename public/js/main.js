c575e201154921f6f602c3c15f2d331d8a19dd15
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-08-31 14:59:41 +0100

    adding a custom store: sort of a backend redux xD

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
+import * as store from "./store.js";
+
 const socket = io("/");
 
 socket.on("connect", () => {
   console.log("succesfully connected to socket.io server");
   console.log(socket.id);
+  store.setSocketId(socket.id);
 });
