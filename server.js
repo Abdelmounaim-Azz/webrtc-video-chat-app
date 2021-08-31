48dfe3da0add96484623ed96a0fa8b4087e52abb
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-08-31 06:18:09 +0100

    connecting server with socket.io

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+io.on("connection", (socket) => {
+  console.log(`client connected to socket.io server with id:${socket.id}`);
 });
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
