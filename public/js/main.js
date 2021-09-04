271c8d70446d906e6a6a47fabcf25792296bd34e
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-09-04 18:28:19 +0100

    finishing reconding functionnality

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ import * as wss from "./wss.js";
 import * as webRTCHandler from "./webRTCHandler.js";
 import * as constants from "./constants.js";
 import * as ui from "./ui.js";
+import * as recordingUtils from "./recordingUtils.js";
 
 // initialization of socketIO connection
 const socket = io("/");
@@ -97,4 +98,31 @@ sendMessageButton.addEventListener("click", () => {
   webRTCHandler.sendMessageUsingDataChannel(message);
   ui.appendMessage(message, true);
   newMessageInput.value = "";
+});
+// recording
+
+const startRecordingButton = document.getElementById("start_recording_button");
+startRecordingButton.addEventListener("click", () => {
+  recordingUtils.startRecording();
+  ui.showRecordingPanel();
+});
+
+const stopRecordingButton = document.getElementById("stop_recording_button");
+stopRecordingButton.addEventListener("click", () => {
+  recordingUtils.stopRecording();
+  ui.resetRecordingButtons();
+});
+
+const pauseRecordingButton = document.getElementById("pause_recording_button");
+pauseRecordingButton.addEventListener("click", () => {
+  recordingUtils.pauseRecording();
+  ui.switchRecordingButtons(true);
+});
+
+const resumeRecordingButton = document.getElementById(
+  "resume_recording_button"
+);
+resumeRecordingButton.addEventListener("click", () => {
+  recordingUtils.resumeRecording();
+  ui.switchRecordingButtons();
 });
